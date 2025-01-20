62eec5680cc772c6094fc8ff1895f873031da71c
lewkAa <tihonovtan@gmail.com>
2025-01-20 22:56:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100; // стартовый баланс
+        int amount = 1200; // сумма пополнения
+        int bonus = amount > 1000 ? amount / 100 : 0;  // начисляемый бонус
+        int totalbalance = balance + amount + bonus;   // итоговый баланс
+
+        System.out.println("Вы успешно пополнили счет на " + amount + " рублей.");
+
+        if (bonus > 0) {
+            System.out.println("Вам начислено " + bonus + " бонусных рублей.");
+        }
+
+        System.out.println("Ваш баланс составляет " + totalbalance + " рублей.");
+
+
+    }
+}
